92375b14c88e9f86bec691eaa7d7f45c1bc54418
Jhon Deivid Rojas Rodriguez <jhonrojasr253@gmail.com>
2025-09-16 15:00:15 -0500

    Creacion de la primer vista para listar todas las personas de la db

--- src/main/java/com/example/primerawebspringboot/controller/PersonaController.java
+++ src/main/java/com/example/primerawebspringboot/controller/PersonaController.java
@@ -5,6 +5,7 @@ import com.example.primerawebspringboot.services.PersonaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
@@ -14,9 +15,12 @@ public class PersonaController {
     @Autowired
     private PersonaService personaService;
 
+    // La clase Model se usa para transferir objetos del controller a la
+    @GetMapping("/")
     public String listarPersonas(Model model){
         List<Persona> personas = personaService.listarPersonas();
-
+        model.addAttribute("personasLista", personas); // clave: valor
+        return "listar";
     }
 
 }
